2cb0e09700619c17fd4f70401605bbb6c220c465
Max <maxthomaschandler@gmail.com>
2024-10-22 14:09:30 -0700

    m

--- post.js
+++ post.js
@@ -1,9 +1,7 @@
 function postToSheet (person, teams, eventCode) {
     var formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSeQdmShRrcnwaXVdH4uKQdxKY9UWA4mtwVmy-4KjZTnZZO0hw/viewform?usp=pp_url&entry.191061956=${person}&entry.277701156=${teams}&entry.319104669=${eventCode}`
 
-    fetch(formUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
+    fetch(formUrl, {mode: "no-cors"})
         .catch(error =>
             console.error('Error:', error)
         )
