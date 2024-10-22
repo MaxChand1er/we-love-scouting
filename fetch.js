d701729c81d970563a1edd024d99908f1d677537
Max <maxthomaschandler@gmail.com>
2024-10-22 10:51:50 -0700

    c

--- fetch.js
+++ fetch.js
@@ -1,11 +1,10 @@
 sheetId = "1_BlmEIzj9PHMkKceLILv7pM2YOgHLWMdL8RnDwN5o0c"
 const sheetLink = 'https://docs.google.com/spreadsheets/d/'+sheetId+'/gviz/tq?tqx=out:json&tq&gid=0'
 
-function fetchSheetData() {
+function fetchSheetCsv() {
     return fetch(sheetLink)
         .then(response => response.text())
         .then(data => {
-            document.querySelector("#output").textContent = data;
             return data; // Return the data for further use
         })
         .catch(error => {
@@ -15,7 +14,8 @@ function fetchSheetData() {
 }
 
 // Run the fetch operation
-fetchSheetData();
+const csv = fetchSheetCsv()
+console.log(csv)
 
 function csvToJson(csvString) {
     const rows = csvString
