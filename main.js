1e1a29678baaab9976190c144ea4e1f326df60b5
Max <maxthomaschandler@gmail.com>
2024-10-22 13:49:43 -0700

    m

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const url = "https://maxchand1er.github.io/religious-scouting-rituals/"
 
 const eventDropdown = document.querySelector("#event-dropdown")
-const eventContainer = document.querySelector("#event-container")
+const eventContainerTemplate = document.querySelector("#event-container")
 
 
 eventDropdown.addEventListener("change", () => {
@@ -9,6 +9,13 @@ eventDropdown.addEventListener("change", () => {
 })
 
 async function SelectEvent(event) {
+    if (document.querySelectorAll(".event-container").length > 1) {
+        document.querySelectorAll(".event-container").forEach(el => {
+            if (el.id != "event-container-template") {
+                el.remove()
+            }
+        });
+    }
     if (event == "") {
         eventContainer.classList.add("hidden")
         return
@@ -16,10 +23,21 @@ async function SelectEvent(event) {
 
     const eventData = await fetch(url + "data.json")
         .then(response => response.json())
-        .then(data => console.log(data))["events"][event]
-    
+        .then(data => console.log(data)["events"][event])
 
-    document.querySelector("#pick-container").querySelector("h1").textContent
-    GetJson()
+    let container = eventContainer.cloneNode(true)
+    container.id = "event-container-" + event
+    
+    container.querySelector("#pick-container").querySelector("h1").textContent = `Make your pick for ${eventData["name"]}!`
+
+    container.querySelector(".pick-submit-btn").addEventListener("click", () => {
+        const name = container.querySelector(".name-input").value.strip()
+        const picks = container.querySelector(".picks-input").value.strip()
+
+        postToSheet(name, picks, event)
+    })
+
+    container.classList.remove("hidden")
+    document.querySelector("main").appendChild(container)
     console.log(event)
 }
--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-function post (person, teams, eventCode) {
+function postToSheet (person, teams, eventCode) {
     var formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSeQdmShRrcnwaXVdH4uKQdxKY9UWA4mtwVmy-4KjZTnZZO0hw/viewform?usp=pp_url&entry.191061956=${person}&entry.277701156=${teams}&entry.319104669=${eventCode}`
 
     fetch(formUrl, {mode: "no-cors"})
